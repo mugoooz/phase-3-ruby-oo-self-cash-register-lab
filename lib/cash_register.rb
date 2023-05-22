1658438bd6b0093faab83b8335138890e6c3d442
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 17:17:31 +0300

    Initialized

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,10 @@
 class CashRegister
+    attr_reader :total, :discount, :items
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
 
 end
