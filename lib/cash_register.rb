95e2582f6b7089233fcd9ae10ac83fd5866c9640
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 17:31:41 +0300

    Debugged code

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,47 +1,39 @@
 class CashRegister
-    attr_reader :total, :discount, :items
-
-    def initialize(discount=0)
-        @total = 0
-        @discount = discount
-        @items = []
+    attr_reader :total, :discount, :items, :last_transaction
+  
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @items = []
+      @last_transaction = {}
     end
-
+  
     def add_item(title, price, quantity = 1)
-        self.total += price * quantity
-        quantity.times { items << title }
+      self.total += price * quantity
+      quantity.times { items << title }
+      self.last_transaction = { title: title, price: price, quantity: quantity }
     end
-
+  
     def apply_discount
-        if discount > 0
-            self.total -= (total * (discount / 100.0)).to_i
-            "After the discount, the total comes to $#{total}."
-        else
-            "There is no discount to apply."
-        end
+      if discount > 0
+        self.total -= (total * (discount / 100.0)).to_i
+        "After the discount, the total comes to $#{total}."
+      else
+        "There is no discount to apply."
+      end
     end
-
+  
     def void_last_transaction
-        last_item_price = items_prices.pop
-        self.total -= last_item_price
+      return unless last_transaction
+  
+      last_item_price = last_transaction[:price] * last_transaction[:quantity]
+      self.total -= last_item_price
+      items.delete(last_transaction[:title])
+      self.last_transaction = nil if items.empty?
     end
-    
-    def total=(value)
-        @total = value
-    end
-
-    def items_prices
-        items.map { |item| item_prices[item] }
-    end
-
-    def items_prices
-        {
-            "apple" => 0.5,
-            "banana" => 0.25,
-            "orange" => 0.75
-        }
-    end
-
-    
-
-end
+  
+   
+  
+    attr_writer :total, :last_transaction
+  end
+  
