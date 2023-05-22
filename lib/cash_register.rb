e40fa6e38583e284c3c56c3cb6a97785a5dba6d1
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 17:26:10 +0300

    Completed lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,4 +7,41 @@ class CashRegister
         @items = []
     end
 
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+        quantity.times { items << title }
+    end
+
+    def apply_discount
+        if discount > 0
+            self.total -= (total * (discount / 100.0)).to_i
+            "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        last_item_price = items_prices.pop
+        self.total -= last_item_price
+    end
+    
+    def total=(value)
+        @total = value
+    end
+
+    def items_prices
+        items.map { |item| item_prices[item] }
+    end
+
+    def items_prices
+        {
+            "apple" => 0.5,
+            "banana" => 0.25,
+            "orange" => 0.75
+        }
+    end
+
+    
+
 end
